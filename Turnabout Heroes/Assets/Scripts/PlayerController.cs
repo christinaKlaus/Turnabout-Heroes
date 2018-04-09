821fa6c6850bb1d1b4ed44c4efc271405f649f5d
Chrissi <klaus.christina@web.de>
2018-04-09 12:55:00 +0200

    Player can let the characters spin

---
+++ Turnabout Heroes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float rotationAmount = 0.5f;
+	Rigidbody rigid;
+
+	void Start(){
+		rigid = GetComponent<Rigidbody>();
+	}
+    void Update()
+    {
+       if(Input.GetButton("TurnLeft")){
+		   Debug.Log("TurnLeft");
+		   rigid.transform.Rotate(new Vector3(0, -rotationAmount, 0));
+	   }
+
+	   if(Input.GetButton("TurnRight")){
+		   Debug.Log("TurnRight");
+		   rigid.transform.Rotate(new Vector3(0, rotationAmount, 0));
+	   }
+    }
+	
+}
