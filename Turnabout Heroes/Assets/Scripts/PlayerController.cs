683dd27cf46c20b8c9f6cbb7940280ea9b89d370
Chrissi <klaus.christina@web.de>
2018-04-09 15:31:37 +0200

    First Bug... manipulating distance resets rotation

--- Turnabout Heroes/Assets/Scripts/PlayerController.cs
+++ Turnabout Heroes/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float rotationAmount = 0.5f;
+	public float distanceAmount = 0.5f;
+	[SerializeField] float maxDistance = 4f;
+	[SerializeField] float minDistance = 1f;
+	[SerializeField] GameObject red;
+	[SerializeField] GameObject blue; 
     void Update()
     {
        if(Input.GetButton("TurnLeft")){
@@ -14,6 +19,18 @@ public class PlayerController : MonoBehaviour {
 	   if(Input.GetButton("TurnRight")){
 		   Debug.Log("TurnRight");
 		   transform.Rotate(new Vector3(0, rotationAmount, 0));
+	   }
+
+	   if(Input.GetButton("DistanceIncrease") && red.transform.position.x <= maxDistance/2){
+		   Debug.Log("DistanceIncrease");
+		   red.transform.position = new Vector3(red.transform.position.x + distanceAmount/2, 0, 0);	   
+	   	   blue.transform.position = new Vector3(blue.transform.position.x - distanceAmount/2, 0, 0);
+	   }
+
+	   if(Input.GetButton("DistanceDecrease") && red.transform.position.x >= minDistance/2){
+		   Debug.Log("DistanceDecrease");
+		   red.transform.position = new Vector3(red.transform.position.x - distanceAmount/2, 0, 0);	   
+	   	   blue.transform.position = new Vector3(blue.transform.position.x + distanceAmount/2, 0, 0);
 	   }
     }
 	
